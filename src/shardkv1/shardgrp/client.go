a9a80f900ce2d46d29f20042f6c06a1d6f3ce58a
Jie Ming <solomon_jie@163.com>
2025-12-20 23:35:06 +0800

    write code for config change to get share migration

--- src/shardkv1/shardgrp/client.go
+++ src/shardkv1/shardgrp/client.go
@@ -6,6 +6,7 @@ import (
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/shardkv1/shardcfg"
+	"6.5840/shardkv1/shardgrp/shardrpc"
 	tester "6.5840/tester1"
 )
 
@@ -31,7 +32,7 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 		server := ck.leaderId
 
 		ok := ck.clnt.Call(ck.servers[server], "KVServer.Get", &args, &reply)
-
+		//log.Printf("Submit Request %T %v Reply: %v", args, args, reply)
 		if ok {
 			if reply.Err == rpc.OK || reply.Err == rpc.ErrNoKey {
 				return reply.Value, reply.Version, reply.Err
@@ -40,6 +41,11 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 			if reply.Err == rpc.ErrWrongLeader {
 				ck.leaderId = (server + 1) % len(ck.servers)
 				continue
+			}
+
+			// 客户端收到 ErrWrongGroup，让上层 ShardKV Client 处理（查询新配置）
+			if reply.Err == rpc.ErrWrongGroup {
+				return "", 0, reply.Err
 			}
 		} else {
 			ck.leaderId = (server + 1) % len(ck.servers)
@@ -57,6 +63,7 @@ func (ck *Clerk) Put(key string, value string, version rpc.Tversion) rpc.Err {
 		server := ck.leaderId
 
 		ok := ck.clnt.Call(ck.servers[server], "KVServer.Put", &args, &reply)
+		//log.Printf("Submit Request %T %v Reply: %v", args, args, reply)
 
 		if ok {
 			if reply.Err == rpc.OK {
@@ -78,6 +85,10 @@ func (ck *Clerk) Put(key string, value string, version rpc.Tversion) rpc.Err {
 				continue
 			}
 
+			if reply.Err == rpc.ErrWrongGroup {
+				return reply.Err
+			}
+
 			ck.leaderId = (server + 1) % len(ck.servers)
 			isFirstTry = false
 
@@ -91,16 +102,95 @@ func (ck *Clerk) Put(key string, value string, version rpc.Tversion) rpc.Err {
 }
 
 func (ck *Clerk) FreezeShard(s shardcfg.Tshid, num shardcfg.Tnum) ([]byte, rpc.Err) {
-	// Your code here
-	return nil, ""
+	args := shardrpc.FreezeShardArgs{Shard: s, Num: num}
+	for {
+		reply := shardrpc.FreezeShardReply{}
+		server := ck.leaderId
+
+		ok := ck.clnt.Call(ck.servers[server], "KVServer.FreezeShard", &args, &reply)
+		//log.Printf("Submit Request %T %v Reply: %v", args, args, reply.Err)
+
+		if ok {
+			if reply.Err == rpc.OK {
+				return reply.State, rpc.OK
+			}
+
+			if reply.Err == rpc.ErrWrongLeader {
+				ck.leaderId = (server + 1) % len(ck.servers)
+				time.Sleep(10 * time.Millisecond)
+				continue
+			}
+
+			// 收到其他错误，返回给 ShardCtrler
+			return nil, reply.Err
+		} else {
+			ck.leaderId = (server + 1) % len(ck.servers)
+		}
+
+		time.Sleep(10 * time.Millisecond)
+	}
 }
 
 func (ck *Clerk) InstallShard(s shardcfg.Tshid, state []byte, num shardcfg.Tnum) rpc.Err {
-	// Your code here
-	return ""
+	args := shardrpc.InstallShardArgs{Shard: s, State: state, Num: num}
+
+	for {
+		reply := shardrpc.InstallShardReply{}
+		server := ck.leaderId
+
+		ok := ck.clnt.Call(ck.servers[server], "KVServer.InstallShard", &args, &reply)
+		//log.Printf("Submit Request %T Num %v - Shard %v Reply: %v", args, args.Num, args.Shard, reply)
+
+		if ok {
+			if reply.Err == rpc.OK {
+				// 成功安装
+				return rpc.OK
+			}
+
+			if reply.Err == rpc.ErrWrongLeader {
+				ck.leaderId = (server + 1) % len(ck.servers)
+				time.Sleep(10 * time.Millisecond)
+				continue
+			}
+
+			// 收到其他错误，返回给 ShardCtrler
+			return reply.Err
+		} else {
+			ck.leaderId = (server + 1) % len(ck.servers)
+		}
+
+		time.Sleep(10 * time.Millisecond)
+	}
 }
 
 func (ck *Clerk) DeleteShard(s shardcfg.Tshid, num shardcfg.Tnum) rpc.Err {
-	// Your code here
-	return ""
+	args := shardrpc.DeleteShardArgs{Shard: s, Num: num}
+	for {
+		reply := shardrpc.DeleteShardReply{}
+		server := ck.leaderId
+
+		ok := ck.clnt.Call(ck.servers[server], "KVServer.DeleteShard", &args, &reply)
+		//log.Printf("Submit Request %T %v Reply: %v", args, args, reply)
+
+		if ok {
+			if reply.Err == rpc.OK {
+				// 成功删除
+				return rpc.OK
+			}
+
+			if reply.Err == rpc.ErrWrongLeader {
+				ck.leaderId = (server + 1) % len(ck.servers)
+				time.Sleep(10 * time.Millisecond)
+				continue
+			}
+
+			// 收到其他错误，返回给 ShardCtrler
+			return reply.Err
+		} else {
+			// RPC 失败 (网络错误)，切换 Leader
+			ck.leaderId = (server + 1) % len(ck.servers)
+		}
+
+		time.Sleep(10 * time.Millisecond)
+	}
 }
--- src/shardkv1/shardgrp/server.go
+++ src/shardkv1/shardgrp/server.go
@@ -2,6 +2,7 @@ package shardgrp
 
 import (
 	"bytes"
+	"log"
 	"sync"
 	"sync/atomic"
 
@@ -27,12 +28,6 @@ const (
 	ShardDeleted                   // 2: Shard 数据已删除 (不接受 Get/Put)
 )
 
-type ShardKVStore struct {
-	ShardID    shardcfg.Tshid
-	ShardState ShardState
-	ShardDB    map[string]KVStore
-}
-
 type KVServer struct {
 	me   int
 	dead int32 // set by Kill()
@@ -42,7 +37,8 @@ type KVServer struct {
 	// Your code here
 	mu           sync.Mutex
 	configNum    shardcfg.Tnum
-	ShardStorage map[shardcfg.Tshid]ShardKVStore
+	shardsState  map[shardcfg.Tshid]ShardState
+	ShardStorage map[shardcfg.Tshid]map[string]KVStore
 }
 
 func (kv *KVServer) DoOp(req any) any {
@@ -52,59 +48,137 @@ func (kv *KVServer) DoOp(req any) any {
 	switch op := req.(type) {
 	case rpc.GetArgs:
 		shardId := shardcfg.Key2Shard(op.Key)
-
-		shardStorage, ok := kv.ShardStorage[shardId]
-		if !ok || shardStorage.ShardState == ShardDeleted {
+		//log.Printf("Receive %T Request: %v", req, req)
+		shardState, ok := kv.shardsState[shardId]
+		if !ok || (shardState != ShardOwned && shardState != ShardFrozen) {
 			return rpc.GetReply{Err: rpc.ErrWrongGroup}
 		}
 
-		if item, ok := shardStorage.ShardDB[op.Key]; ok {
+		shardData := kv.ShardStorage[shardId]
+		if item, ok := shardData[op.Key]; ok {
 			return rpc.GetReply{Value: item.Value, Version: item.Version, Err: rpc.OK}
 		} else {
 			return rpc.GetReply{Err: rpc.ErrNoKey}
 		}
 	case rpc.PutArgs:
 		shardId := shardcfg.Key2Shard(op.Key)
-
-		shardStorage, ok := kv.ShardStorage[shardId]
-		if !ok || shardStorage.ShardState == ShardDeleted {
-			//return rpc.PutReply{Err: rpc.ErrWrongGroup}
-			// This version will build shardID
-			kv.BuildShard(shardId)
+		shardState, ok := kv.shardsState[shardId]
+		if !ok || shardState != ShardOwned {
+			return rpc.PutReply{Err: rpc.ErrWrongGroup}
 		}
-		shardStorage = kv.ShardStorage[shardId]
 
-		item, ok := shardStorage.ShardDB[op.Key]
+		shardData := kv.ShardStorage[shardId]
+		item, ok := shardData[op.Key]
 
 		if ok && item.Version != op.Version {
 			return rpc.PutReply{Err: rpc.ErrVersion}
 		}
 
 		newVersion := op.Version + 1
-		shardStorage.ShardDB[op.Key] = KVStore{
+		shardData[op.Key] = KVStore{
 			Value:   op.Value,
 			Version: newVersion,
 		}
 		return rpc.PutReply{Err: rpc.OK}
+	case shardrpc.DeleteShardArgs:
+
+		return kv.applyDelete(op)
+	case shardrpc.FreezeShardArgs:
+		return kv.applyFreeze(op)
+	case shardrpc.InstallShardArgs:
+		return kv.applyInstall(op)
 	default:
 		return nil
 	}
 }
 
-func (kv *KVServer) BuildShard(shardID shardcfg.Tshid) {
-	_, ok := kv.ShardStorage[shardID]
-
-	if ok {
-		return
+func (kv *KVServer) applyInstall(args shardrpc.InstallShardArgs) shardrpc.InstallShardReply {
+	if args.Num < kv.configNum {
+		return shardrpc.InstallShardReply{Err: rpc.ErrWrongGroup}
 	}
 
-	shardStorage := ShardKVStore{
-		ShardID:    shardID,
-		ShardState: ShardOwned,
-		ShardDB:    make(map[string]KVStore),
+	kv.configNum = args.Num
+	//kv.BuildShard(args.Shard)
+	shardStorage := kv.deserializeShardData(args.State)
+	kv.ShardStorage[args.Shard] = shardStorage
+	kv.shardsState[args.Shard] = ShardOwned
+
+	return shardrpc.InstallShardReply{Err: rpc.OK}
+}
+
+func (kv *KVServer) applyFreeze(args shardrpc.FreezeShardArgs) shardrpc.FreezeShardReply {
+	if args.Num < kv.configNum {
+		return shardrpc.FreezeShardReply{Num: kv.configNum, Err: rpc.ErrWrongGroup}
 	}
 
-	kv.ShardStorage[shardID] = shardStorage
+	shardState, ok := kv.shardsState[args.Shard]
+
+	if !ok || shardState == ShardDeleted {
+		// 不拥有此分片，无法冻结
+		return shardrpc.FreezeShardReply{Num: kv.configNum, Err: rpc.ErrWrongGroup}
+	}
+
+	// 收集并序列化数据
+	state := kv.serializeShardData(args.Shard)
+
+	// 状态切换：只有当请求的 Num 大于当前配置时才更新状态
+	if args.Num > kv.configNum {
+		kv.shardsState[args.Shard] = ShardFrozen
+	}
+
+	return shardrpc.FreezeShardReply{State: state, Num: args.Num, Err: rpc.OK}
+}
+
+func (kv *KVServer) applyDelete(args shardrpc.DeleteShardArgs) shardrpc.DeleteShardReply {
+	if args.Num < kv.configNum {
+		return shardrpc.DeleteShardReply{Err: rpc.ErrWrongGroup}
+	}
+
+	shardState, ok := kv.shardsState[args.Shard]
+
+	if ok && shardState == ShardOwned {
+		// 拒绝删除当前 Owned 的分片
+		return shardrpc.DeleteShardReply{Err: rpc.ErrWrongGroup}
+	}
+
+	// 幂等性：如果已经是 Deleted，则允许重复操作。
+	if ok && shardState == ShardDeleted {
+		return shardrpc.DeleteShardReply{Err: rpc.OK}
+	}
+
+	// 执行删除
+	delete(kv.ShardStorage, args.Shard)
+	kv.shardsState[args.Shard] = ShardDeleted
+
+	return shardrpc.DeleteShardReply{Err: rpc.OK}
+}
+
+func (kv *KVServer) serializeShardData(shardId shardcfg.Tshid) []byte {
+	shardData, ok := kv.ShardStorage[shardId]
+	if !ok {
+		// 如果没有数据，返回空 map 的序列化
+		shardData = make(map[string]KVStore)
+	}
+
+	w := new(bytes.Buffer)
+	e := labgob.NewEncoder(w)
+
+	if err := e.Encode(shardData); err != nil {
+		panic(err)
+	}
+	return w.Bytes()
+}
+
+func (kv *KVServer) deserializeShardData(data []byte) map[string]KVStore {
+	r := bytes.NewBuffer(data)
+	d := labgob.NewDecoder(r)
+
+	var ShardStorage map[string]KVStore
+	if err := d.Decode(&ShardStorage); err != nil {
+		log.Printf("Deserialize failed: %v", err)
+		return make(map[string]KVStore)
+	}
+	return ShardStorage
 }
 
 func (kv *KVServer) Snapshot() []byte {
@@ -117,10 +191,13 @@ func (kv *KVServer) Snapshot() []byte {
 	if err := e.Encode(kv.configNum); err != nil {
 		panic(err)
 	}
-
+	if err := e.Encode(kv.shardsState); err != nil {
+		panic(err)
+	}
 	if err := e.Encode(kv.ShardStorage); err != nil {
 		panic(err)
 	}
+
 	return w.Bytes()
 }
 
@@ -136,22 +213,27 @@ func (kv *KVServer) Restore(data []byte) {
 	d := labgob.NewDecoder(r)
 
 	var nextConfigNum shardcfg.Tnum
-	var nextDb map[shardcfg.Tshid]ShardKVStore
+	var nextShardsState map[shardcfg.Tshid]ShardState
+	var nextShardsData map[shardcfg.Tshid]map[string]KVStore
 
 	if err := d.Decode(&nextConfigNum); err != nil {
 		panic(err)
 	}
-	if err := d.Decode(&nextDb); err != nil {
+	if err := d.Decode(&nextShardsState); err != nil {
+		panic(err)
+	}
+	if err := d.Decode(&nextShardsData); err != nil {
 		panic(err)
 	}
 
 	kv.configNum = nextConfigNum
-	kv.ShardStorage = nextDb
+	kv.shardsState = nextShardsState
+	kv.ShardStorage = nextShardsData
 }
 
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	err, res := kv.rsm.Submit(*args)
-
+	//log.Printf("Get: %v", args)
 	if err == rpc.ErrWrongLeader {
 		reply.Err = rpc.ErrWrongLeader
 		return
@@ -169,7 +251,7 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 	err, res := kv.rsm.Submit(*args)
-
+	//log.Printf("Put: %v", args)
 	if err == rpc.ErrWrongLeader {
 		reply.Err = rpc.ErrWrongLeader
 		return
@@ -188,17 +270,70 @@ func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 // Freeze the specified shard (i.e., reject future Get/Puts for this
 // shard) and return the key/values stored in that shard.
 func (kv *KVServer) FreezeShard(args *shardrpc.FreezeShardArgs, reply *shardrpc.FreezeShardReply) {
-	// Your code here
+	err, res := kv.rsm.Submit(*args)
+	//log.Printf("FreezeShard: %v", args)
+	if err == rpc.ErrWrongGroup {
+		reply.Err = rpc.ErrWrongGroup
+		return
+	}
+
+	if err != rpc.OK {
+		reply.Err = err
+		return
+	}
+
+	rep, ok := res.(shardrpc.FreezeShardReply)
+	if !ok {
+		reply.Err = rpc.ErrWrongLeader
+		return
+	}
+	*reply = rep
 }
 
 // Install the supplied state for the specified shard.
 func (kv *KVServer) InstallShard(args *shardrpc.InstallShardArgs, reply *shardrpc.InstallShardReply) {
-	// Your code here
+	err, res := kv.rsm.Submit(*args)
+	//log.Printf("InstallShard: %v", args)
+
+	if err == rpc.ErrWrongGroup {
+		reply.Err = rpc.ErrWrongGroup
+		return
+	}
+
+	if err != rpc.OK {
+		reply.Err = err
+		return
+	}
+
+	rep, ok := res.(shardrpc.InstallShardReply)
+	if !ok {
+		reply.Err = rpc.ErrWrongLeader
+		return
+	}
+	*reply = rep
 }
 
 // Delete the specified shard.
 func (kv *KVServer) DeleteShard(args *shardrpc.DeleteShardArgs, reply *shardrpc.DeleteShardReply) {
-	// Your code here
+	//log.Printf("DeleteShard: %v", args)
+	err, res := kv.rsm.Submit(*args)
+
+	if err == rpc.ErrWrongLeader {
+		reply.Err = rpc.ErrWrongLeader
+		return
+	}
+
+	if err != rpc.OK {
+		reply.Err = err
+		return
+	}
+
+	rep, ok := res.(shardrpc.DeleteShardReply)
+	if !ok {
+		reply.Err = rpc.ErrWrongLeader
+		return
+	}
+	*reply = rep
 }
 
 // the tester calls Kill() when a KVServer instance won't
@@ -233,14 +368,30 @@ func StartServerShardGrp(servers []*labrpc.ClientEnd, gid tester.Tgid, me int, p
 	labgob.Register(shardrpc.DeleteShardArgs{})
 	labgob.Register(rsm.Op{})
 	labgob.Register(KVStore{})
-	labgob.Register(ShardKVStore{})
+	labgob.Register(ShardState(0))
+	labgob.Register(map[string]KVStore{})
+	labgob.Register(make(map[shardcfg.Tshid]map[string]KVStore))
 
 	kv := &KVServer{
 		gid:          gid,
 		me:           me,
 		configNum:    0,
-		ShardStorage: make(map[shardcfg.Tshid]ShardKVStore),
+		shardsState:  make(map[shardcfg.Tshid]ShardState),
+		ShardStorage: make(map[shardcfg.Tshid]map[string]KVStore),
 	}
+
+	for i := 0; i < shardcfg.NShards; i++ {
+		shardID := shardcfg.Tshid(i)
+		if gid == shardcfg.Gid1 {
+			// 默认第一个 Group 拥有所有分片
+			kv.shardsState[shardID] = ShardOwned
+			kv.ShardStorage[shardID] = make(map[string]KVStore)
+		} else {
+			// 其他 Group 默认不拥有任何分片
+			kv.shardsState[shardID] = ShardDeleted
+		}
+	}
+
 	kv.rsm = rsm.MakeRSM(servers, me, persister, maxraftstate, kv)
 
 	return []tester.IService{kv, kv.rsm.Raft()}
